11bf860d4cd5a0506196d01d69c86db2e25d0efc
sherlore <sherlore@gmail.com>
2019-07-27 15:04:27 +0800

    Level
    Lv2
    Lv3
    Lv4

---
+++ Assets/Lv4Console.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lv4Console : MonoBehaviour
+{
+	public GameObject[] girls;
+	public int index;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<girls.Length; i++)
+		{
+			girls[i].SetActive(false);
+		}
+		index = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") )
+		{
+			if(index >= 0 )
+				girls[index].SetActive(false);
+			
+			
+			if(index < girls.Length )
+			{
+				index++;
+				girls[index].SetActive(true);
+			}
+		}
+    }
+}
---
+++ Assets/TestConsole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestConsole : MonoBehaviour
+{
+	public Text camPos;
+	public Transform cam;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camPos.text = System.String.Format("Cam: {0}", cam.position);
+    }
+}
