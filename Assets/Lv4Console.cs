e135b7bd4d398cdcc9a6486d7d3c07d20f5fb621
sherlore <sherlore@gmail.com>
2019-07-27 16:11:23 +0800

    Level Complete
    Lv2,3,4

--- Assets/Lv4Console.cs
+++ Assets/Lv4Console.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Lv4Console : MonoBehaviour
 {
 	public GameObject[] girls;
+	public GameObject tvScreen;
+	public GameObject tvNoise;
+	
 	public int index;
 	
     // Start is called before the first frame update
@@ -14,15 +17,70 @@ public class Lv4Console : MonoBehaviour
 		{
 			girls[i].SetActive(false);
 		}
-		index = -1;
+		// index = -1;
+		// StartCoroutine(TVShow());
+		SetNoise(false);
     }
+	
+	
+	void SetNoise(bool val)
+	{
+      tvScreen.SetActive(!val);
+	  tvNoise.SetActive(val);
+	}
+
+   IEnumerator TVShow()
+   {
+	  
+	  int i = 0;
+	  
+	  SetNoise(false);
+      yield return new WaitForSeconds(2.0f);
+	  
+	  SetNoise(true);
+	  // girls[index].SetActive(false);
+      yield return new WaitForSeconds(1.0f);
+	  girls[i].SetActive(true);
+	  
+	  SetNoise(false);
+      yield return new WaitForSeconds(2.0f);
+	  
+	  SetNoise(true);
+	  girls[i].SetActive(false);
+	  i++;
+      yield return new WaitForSeconds(1.0f);
+	  girls[i].SetActive(true);
+	  
+	  SetNoise(false);
+      yield return new WaitForSeconds(2.0f);
+	  
+	  SetNoise(true);
+	  girls[i].SetActive(false);
+	  i++;
+      yield return new WaitForSeconds(1.0f);
+	  girls[i].SetActive(true);
+	  
+	  SetNoise(false);
+      yield return new WaitForSeconds(2.0f);
+	  
+	  SetNoise(true);
+	  girls[i].SetActive(false);
+	  i++;
+      yield return new WaitForSeconds(1.0f);
+	  girls[i].SetActive(true);
+	  
+	  SetNoise(false);
+      yield return new WaitForSeconds(2.0f);
+     
+   }
+	
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Jump") )
 		{
-			if(index >= 0 )
+			/*if(index >= 0 )
 				girls[index].SetActive(false);
 			
 			
@@ -30,7 +88,9 @@ public class Lv4Console : MonoBehaviour
 			{
 				index++;
 				girls[index].SetActive(true);
-			}
+			}*/
+			
+			StartCoroutine(TVShow());
 		}
     }
 }
