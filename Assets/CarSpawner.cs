71f9d811a817815a038bc54d3972c1cd7c070829
yoyotvyoo <yoyotvyoo@gmail.com>
2019-07-28 01:15:11 +0800

    Level 7 completed

---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] cars;
+    public Transform start;
+    public Transform end;
+
+    void Start()
+    {
+        InvokeRepeating("LaunchCar", 2.0f, 5.0f);
+    }
+
+    void LaunchCar()
+    {
+        var index = Random.Range(0, cars.Length);
+
+        var go = GameObject.Instantiate(cars[index], start.position, start.rotation);
+        go.GetComponent<MoveTowards>().destination = end;
+    }
+}
