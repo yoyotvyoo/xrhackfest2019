431aca0073ea59393e1b5d01a8650dd4dc9e1c8c
sherlore <sherlore@gmail.com>
2019-08-28 09:55:32 +0800

    XR 20190828

---
+++ Assets/CamShake.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class CamShake : MonoBehaviour
+{
+	public float magnitude;
+	public float roughness;
+	public float fadeInTime;
+	public float fadeOutTime;
+	public Vector3 posInfluence;
+	public Vector3 rotInfluence;
+	
+	public Transform cam;
+	public Transform camShell;
+	
+	public AudioSource myAudio;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Invoke("AttackShell", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2") )
+		{
+			Shake();
+		}
+    }
+	
+	public void AttackShell()
+	{
+		// camShell.position = cam.position;
+		// cam.parent = camShell;
+	}
+	
+	public void Shake()
+	{
+		CameraShaker.Instance.ShakeOnce(magnitude, roughness, fadeInTime, fadeOutTime, posInfluence, rotInfluence);
+		myAudio.Play();
+	}
+}
---
+++ Assets/Wheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Wheel : MonoBehaviour
+{
+	public Vector3 angularSpeed;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(angularSpeed * Time.deltaTime);
+    }
+}
